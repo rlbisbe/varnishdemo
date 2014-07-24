0c2f6c1ffb09bc9fba3410e1e9775a077ac24231
Roberto Luis <rober90@gmail.com>
2014-07-25 01:09:42 +0200

    Added all content

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Mvc.Ajax;
+
+namespace varnishdemo.Controllers
+{
+	public class HomeController : Controller
+	{
+		public ActionResult Index ()
+		{
+			ViewData ["now"] = String.Format ("{0:HH:mm:ss}", DateTime.Now);
+			System.Threading.Thread.Sleep (3000);
+			return View ("Index");
+		}
+	}
+}
+
---
+++ Controllers/LayoutController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace varnishdemo.Controllers
+{
+    public class LayoutController : Controller
+    {
+        public ActionResult Sidebar()
+        {
+			ViewData ["now"] = String.Format ("{0:HH:mm:ss}", DateTime.Now);
+			System.Threading.Thread.Sleep (3000);
+            return View ("Sidebar");
+        }
+
+		public ActionResult Header()
+		{
+			ViewData ["now"] = String.Format ("{0:HH:mm:ss}", DateTime.Now);
+			System.Threading.Thread.Sleep (3000);
+			return View ("Header");
+		}
+    }
+}
